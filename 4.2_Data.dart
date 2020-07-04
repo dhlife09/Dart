1e25a45e015787e9e3eae52c9cf960823e7bca06
Dohyun Park <dhlife09@gmail.com>
2020-07-04 18:58:59 +0900

    자료형 :D

---
+++ 4.2_Data.dart
@@ -0,0 +1,22 @@
+/*
+
+Dart에서의 자료형
+
+int: 정수
+double: 부동 소수점 숫자
+String: 문자열
+bool: 불리언(참과 거짓)
+List: 리스트(중복 허용, 순서가 있는 자료구조)
+
+*/
+
+int a = 1;
+double b = 1.2;
+String c = 'Hello?';
+bool d = true;
+List<int> numbers = [1, 2, 3, 2];
+List<String> strings = ['A', 'B', 'C', 'A'];
+
+void main() {
+  print(":D");
+}
