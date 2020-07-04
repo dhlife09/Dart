3aeb82fe450a4b8ff9ca2dafd70fd7923a9b4b64
Dohyun Park <dhlife09@gmail.com>
2020-07-04 19:25:10 +0900

    🍎

---
+++ 4.4_if.dart
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+var apple = 32;
+
+void main() {
+  if ( apple == 5 ) {
+    print('There are 5 apples!');
+  } else if ( apple == 3 ) {
+    print('There are 3 apples!');
+  } else {
+    print('Hmm.. how many apples exists?');
+  }
+}
